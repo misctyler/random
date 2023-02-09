4de123135f76ab0c99617ad7e9bbcd478925a57f
MiscTyler <MikeTylerMosier@gmail.com>
2023-02-09 14:57:23 -0600

    basic keras app

---
+++ keras_demo.py
@@ -0,0 +1,20 @@
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Create a dataset
+x = np.array([[1], [2], [3], [4]])
+y = np.array([[0], [-1], [-2], [-3]])
+
+# Create a model
+model = Sequential()
+model.add(Dense(units=1, input_shape=(1,)))
+
+# Compile the model
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# Train the model
+model.fit(x, y, epochs=100)
+
+# Use the model to make predictions
